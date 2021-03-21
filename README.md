64a3bec8950ad0ab6bd3a1f17533936e7a3e0928
Cuchillo <64203440+Cuchillos@users.noreply.github.com>
2021-03-21 17:01:02 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cabeceras-web
  Obtener las cabeceras de una web
+
+
+Como usarlo:
+-------------
+Termux:
+```
+git clone https://github.com/Cuchillos/cabeceras-web
+cd cabeceras-web-t.py
+pip install -r requirements.txt
+python cabeceras-web.py
+```
+
+PC:
+´´´
+git clone https://github.com/Cuchillos/cabeceras-web
+cd cabeceras-web3.py
+pip install -r requirements.txt
+python cabeceras-web.py
+´´´
---
+++ cabeceras-web-t.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+import requests
+import argparse
+import os
+
+def banner():
+    os.system ("clear")
+
+    print(chr(27)+"[1;31m"+"░█████╗░██╗░░░██╗░█████╗░██╗░░██╗██╗██╗░░░░░██╗░░░░░░█████╗░")
+    print(chr(27)+"[1;31m"+"██╔══██╗██║░░░██║██╔══██╗██║░░██║██║██║░░░░░██║░░░░░██╔══██╗")
+    print(chr(27)+"[1;31m"+"██║░░╚═╝██║░░░██║██║░░╚═╝███████║██║██║░░░░░██║░░░░░██║░░██║")
+    print(chr(27)+"[1;31m"+"██║░░██╗██║░░░██║██║░░██╗██╔══██║██║██║░░░░░██║░░░░░██║░░██║")
+    print(chr(27)+"[1;31m"+"╚█████╔╝╚██████╔╝╚█████╔╝██║░░██║██║███████╗███████╗╚█████╔╝")
+    print(chr(27)+"[1;31m"+"░╚════╝░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚══════╝╚══════╝░╚════╝░")
+    print()
+    print()
+    print(chr(27)+"[1;37m"+"Scanner de cabeceras web")
+    print()
+
+parser = argparse.ArgumentParser(description=banner())
+parser.add_argument('-w','--web',help="Indica la página web")
+parser = parser.parse_args()
+
+def main():
+    if parser.web:
+        try:
+            url = requests.get(url=parser.web)
+            cabeceras = dict(url.headers)
+            for x in cabeceras:
+                print(x,' : ', cabeceras[x])
+        except:
+            print("ERROR: No me he podido acceder :c")
+    else:
+        print("Indica alguna página web :v")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Saliendo...")
+        exit()
